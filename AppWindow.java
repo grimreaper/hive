149a9613fcae45bccffe86031b1e158d0f8d0051
Yitzchak Schwarz <s.y.schwarz@gmail.com>
2010-12-26 21:23:39 -0500

    add java files

---
+++ AppWindow.java
@@ -0,0 +1,14 @@
+package hive;
+
+import javax.swing.JFrame;
+
+public class AppWindow {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+	}
+
+}
---
+++ GraphicalBoard.java
@@ -0,0 +1,17 @@
+package hive;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class GraphicalBoard extends JPanel {
+
+	private static final long serialVersionUID = 6458263485049274169L;
+	
+	private Board board;
+	
+	@Override
+	public void paint(Graphics g) {
+	}
+	
+}
